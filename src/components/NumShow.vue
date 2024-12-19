64c9614c6f911cabd75ef85039ca561b56a36173
Weijun Hu <949920389@qq.com>
2024-12-19 10:25:07 +0800

    Add function to show the four cameras data in NumShow.vue

--- src/components/NumShow.vue
+++ src/components/NumShow.vue
@@ -1,38 +1,34 @@
 <template>
     <div class="now numCon">
-        <div class="nowpeople">当前人数：</div>
-        <div class="nowpeoplenum">{{ data.nowpeople }} 人</div>
-        <div class="pro">
-            <el-progress :percentage="parseInt(data.nowpeople) / 100" :color="data.colors" />
-        </div>
-        <div class="peo">可承载人数：{{ data.nowpeople }}人/10000人</div>
+        <div class="nowpeople">当前累计人数：</div>
+        <div class="nowpeoplenum">{{ data.sum }} 人</div>
     </div>
     <div class="today numCon icon">
         <img src="../assets/day.svg" style="width: 40px;height: 40px;">
-        <div class="numright">
-            <div>当天人次</div>
-            <div class="numin">{{ data.todaypeople }}人</div>
-        </div>
+      <div class="numright">
+        <div>摄像头1</div>
+        <div class="numin">{{ data.camera1Data }}人</div>
+      </div>
     </div>
     <div class="week numCon icon">
         <img src="../assets/week.svg" style="width: 40px;height: 40px;">
         <div class="numright">
-            <div>本周人次</div>
-            <div class="numin">{{ data.monthpeople }}人</div>
+          <div>摄像头2</div>
+          <div class="numin">{{ data.camera2Data }}人</div>
         </div>
     </div>
     <div class="month numCon icon">
         <img src="../assets/month.svg" style="width: 45px;height: 45px;">
         <div class="numright">
-            <div>本月人次</div>
-            <div class="numin">{{ data.monthpeople }}人</div>
+          <div>摄像头3</div>
+          <div class="numin">{{ data.camera3Data }}人</div>
         </div>
     </div>
     <div class="en numCon icon">
         <img src="../assets/sum.svg" style="width: 45px;height: 45px;">
         <div class="numright">
-            <div>累计人次</div>
-            <div class="numin">{{ data.sum }}人</div>
+          <div>摄像头4</div>
+          <div class="numin">{{ data.camera4Data }}人</div>
         </div>
     </div>
     <div class="en numCon icon">
@@ -58,12 +54,15 @@ export default {
         const store = useStore()
         const router = useRouter()
         let data = reactive({
-            nowpeople: 0,
-            todaypeople: 0,
-            monthpeople: 0,
-            sum: 0,             //累计总人数
-            timeold: "",
-            inter:"",
+          nowpeople: 0,
+          todaypeople: 0,
+          monthpeople: 0,
+          sum: 0,
+          inter: "",
+          camera1Data: 0,
+          camera2Data: 0,
+          camera3Data: 0,
+          camera4Data: 0,
             colors: [
                 { color: '#f56c6c', percentage: 20 },
                 { color: '#e6a23c', percentage: 40 },
@@ -71,66 +70,34 @@ export default {
                 { color: '#1989fa', percentage: 80 },
                 { color: '#6f7ad3', percentage: 100 },
             ]
-
-
         })
 
-        
-
         function getFormattedDate() {
-            const now = new Date();
-            const year = now.getFullYear();
-            const month = now.getMonth() + 1;
-            const day = now.getDate();
-            const date = `${year}-${month}-${day}`;
-            return date;
+          return "2024-12-11";
         }
-
-
-        function getFormattedMonth() {
-            const now = new Date();
-            const year = now.getFullYear();
-            const month = now.getMonth() + 1 <= 9 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1);
-            const date = `${year}-${month}`;
-
-            return date;
-        }
-
 
         function init() {
-            axios.get('api/detection/nowpeople').then(res => {
-                data.nowpeople = res.data;
+          axios.get('api/detection/nowpeople').then(res => {
+            data.nowpeople = res.data;
+          }, err => {
+            console.log(err);
+            if (err.response.status == 401) {
+              router.push("login");
+            }
+          })
 
-
-            }, err => {
-                console.log(err);
-                if (err.response.status == 401) {          //未登录，重定向到登录页面
-                    router.push("login");
-                }
-            })
-
-
-            //获取当天的数据
-            axios.get('api/detection/perday/' + getFormattedDate()).then(res => {
-                data.todaypeople = res.data[0].reduce((a, b) => a + b, 0)   //通过reduce对数组求和
-            }, err => {
-                console.log(err);
-                if (err.response.status == 401) {          //未登录，重定向到登录页面
-                    router.push("login");
-                }
-            })
-
-
-            //获取当月的数据
-            axios.get('api/detection/permonth/' + getFormattedMonth()).then(res => {
-                data.monthpeople = res.data[0].reduce((a, b) => a + b, 0)   //通过reduce对数组求和
-
-            }, err => {
-                console.log(err);
-                if (err.response.status == 401) {          //未登录，重定向到登录页面
-                    router.push("login");
-                }
-            })
+          // 获取数据
+          axios.get('api/detection/perday/' + getFormattedDate()).then(res => {
+            data.camera1Data = res.data[1] ? res.data[1].reduce((sum, value) => sum + value, 0) : 0; // 摄像头1的总数
+            data.camera2Data = res.data[2] ? res.data[2].reduce((sum, value) => sum + value, 0) : 0; // 摄像头1的总数
+            data.camera3Data = res.data[3] ? res.data[3].reduce((sum, value) => sum + value, 0) : 0; // 摄像头1的总数
+            data.camera4Data = res.data[4] ? res.data[4].reduce((sum, value) => sum + value, 0) : 0; // 摄像头1的总数
+          }, err => {
+            console.log(err);
+            if (err.response.status == 401) {
+              router.push("login");
+            }
+          })
 
             //获取累计总人数
             axios.get('api/detection/sum/').then(res => {
@@ -142,12 +109,9 @@ export default {
                 }
             })
 
-
-
-
             //计算运行时间
             var dateObj = new Date();   //当前时间
-            var grt = new Date("1/1/2023 00:00:00");//建站时间
+            var grt = new Date("11/12/2024 00:00:00");//建站时间
 
             //计算运行时间
             var days = (dateObj - grt) / 1000 / 60 / 60 / 24;
@@ -172,7 +136,6 @@ export default {
 
         }
 
-
         //生命周期函数，
         //onMounted：在初始化页面完成后执行
         onMounted(() => {
@@ -184,19 +147,12 @@ export default {
             clearInterval(data.inter);
         })
 
-
-
-
         //数据和函数都要返回
         return {
             data,
 
         }
     },
-
-
-
-
 }
 </script>
 <style scoped>
